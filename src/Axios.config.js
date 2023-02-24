3de73cae0af35eb56459f56b52bec4d7f2a69588
Romesh Kosme <romesh.30@gmail.com>
2023-02-24 19:03:44 +0530

    testingForLive

--- src/Axios.config.js
+++ src/Axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://chatbackend-production-c208.up.railway.app/",
 });
 
 // Add a request interceptor
